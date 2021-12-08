4835295488757d214dfd9d585d45869c4b423319
Nova-archx86 <moorcode66@yahoo.com>
2021-12-07 20:16:32 -0800

    status command now displays player names

--- cogs/status.py
+++ cogs/status.py
@@ -15,10 +15,17 @@ class Status(commands.Cog):
         try: 
             server = MinecraftServer.lookup(self.server_address)
             status = server.status()
+            
+            if status.players.sample is not None: 
+                players_names = str(f"{player.name} " for player in status.players.sample)
+            else:
+                players_names = "No players online"
+
             online_embed = discord.Embed(title='Online', color=discord.Color.green())
             online_embed.add_field(name='Version', value=f'{status.version.name}')
+            online_embed.add_field(name='Description', value=f'{status.description}') 
             online_embed.add_field(name='Latency', value=f'{status.latency}ms')
-            online_embed.add_field(name='Players online', value=f'{status.players.online}')
+            online_embed.add_field(name='Players online', value=f'{status.players.online}/{status.players.max}: {players_names}')
             await ctx.send(embed=online_embed)
         except Exception as err:
             offline_embed = discord.Embed(title='Error', description='Could not connect to the server!', color=discord.Color.red())
