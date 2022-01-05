ed7d707a76b307a806afa1cdb6be636e3e607ab8
Nova-archx86 <moorcode66@yahoo.com>
2022-01-05 14:52:07 -0800

    formatted the output better

--- cogs/status.py
+++ cogs/status.py
@@ -17,15 +17,16 @@ class Status(commands.Cog):
             status = server.status()
             
             if status.players.sample is not None: 
-                players_names = str([f"{player.name} " for player in status.players.sample])
+                player_names = [f"{player.name} " for player in status.players.sample]
+                online_players = ", ".join(player_names[:])
             else:
-                players_names = "No players online"
+                online_players = "No players online"
 
             online_embed = discord.Embed(title='Online', color=discord.Color.green())
             online_embed.add_field(name='Version', value=f'{status.version.name}')
             online_embed.add_field(name='Description', value=f'{status.description}') 
             online_embed.add_field(name='Latency', value=f'{status.latency}ms')
-            online_embed.add_field(name='Players online', value=f'{status.players.online}/{status.players.max}: {players_names}')
+            online_embed.add_field(name='Players online', value=f'{status.players.online}/{status.players.max}: {online_players}')
             await ctx.send(embed=online_embed)
         except Exception as err:
             offline_embed = discord.Embed(title='Error', description='Could not connect to the server!', color=discord.Color.red())
