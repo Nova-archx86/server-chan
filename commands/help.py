21663aa7da3ad9ddd5d2249eb40f613737113dd7
Nova-archx86 <moorcode66@yahoo.com>
2022-05-14 13:01:05 -0700

    removed the meme command

--- commands/help.py
+++ commands/help.py
@@ -12,7 +12,6 @@ class Help(commands.Cog):
         embed = discord.Embed(title='List of commands')
         embed.add_field(name='prefix', value='$', inline=False)
         embed.add_field(name='help', value='Sends this message', inline=False)
-        embed.add_field(name='meme', value='sends a meme from the r/memes subreddit', inline=False)
         embed.add_field(name='status', value='Gets the current status from the minecraft server (online players and latency)', inline=False)
         embed.add_field(name='play', value='Downloads and plays a youtube url using the yt-dlp project', inline=False)
         embed.add_field(name='pause', value='Pauses the current audio player', inline=False)
