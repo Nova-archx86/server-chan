71866a6c6af60c3baf0942d72055139f9f8ea45c
Nova-archx86 <moorcode66@yahoo.com>
2022-09-02 23:06:07 -0700

    Replaced old logging method with discord's logging method

--- bot.py
+++ bot.py
@@ -1,24 +1,21 @@
 import discord
-import logging
 import os
 import asyncio
 from discord.ext import commands
+
+discord.utils.setup_logging()
 
 async def load():
     for file in os.listdir('./commands'):
         if file.endswith('.py'): 
             await client.load_extension(f'commands.{file[:-3]}')
 
-# Loads all of the command cogs from the commands folder
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log',encoding='utf-8',mode='w')
 client = commands.Bot(command_prefix='$', help_command=None, intents=discord.Intents.all())
 token = os.environ['TOKEN_1']
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.CustomActivity('Looking for bugs'))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('Python 3.10'))
     print('online!')
 
 async def main():
