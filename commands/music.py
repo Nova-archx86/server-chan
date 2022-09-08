e893987d4176cbce1be95f9c46d0b0ae8d2a7b86
Nova-archx86 <moorcode66@yahoo.com>
2022-09-08 10:49:46 -0700

    Continued working on skip command

--- commands/music.py
+++ commands/music.py
@@ -2,8 +2,6 @@ import discord
 import os
 import yt_dlp
 import logging
-import time
-import threading
 from discord.ext import commands
 from discord import FFmpegPCMAudio
 from yt_dlp import YoutubeDL
@@ -21,11 +19,14 @@ class MusicPlayer(commands.Cog):
         }
         self.queue = []
 
-    main_thread = threading.Thread(target=self.play(), args=(ctx, url))
-    os.chdir('./music') 
-    def clean_up(self, song):
-        os.remove(f'./{song}')
-        self.queue.pop(0) 
+    # os.chdir('./music') 
+    
+    def clean_up(self, title=None):
+        if title: 
+            os.remove(f'./music/{title}')
+
+        if len(self.queue) > 0: 
+            self.queue.pop(0) 
 
     async def queue_player(self, ctx, title, player:FFmpegPCMAudio): 
         self.queue.append(player)
@@ -39,33 +40,38 @@ class MusicPlayer(commands.Cog):
     async def download(self, ctx, url):
         with YoutubeDL(self.ytdlp_options) as ytdl:
             try: 
+                os.chdir('./music') 
                 await ctx.send('Downloading audio...') 
                 ytdl.download([url])
+                os.chdir('../')
             except yt_dlp.DownloadError:
                 await ctx.send('Failed to download audio!')
                 
     @commands.command()
     async def play(self, ctx, url:str):
         if ctx.message.author.voice: 
-            voice_clients = discord.utils.get(self.client.voice_clients, guild=ctx.guild) 
-            if voice_clients == None:
-                channel = ctx.message.author.voice.channel 
-                await channel.connect()
-
                 logging.info(f'Current queue: {self.queue}') 
-
-                if ctx.voice_client.is_playing():
+                await self.join(ctx)
+                
+                if ctx.voice_client.is_playing(): 
                     await self.download(ctx, url) 
                     title = self.get_title(ctx, url) 
-                    self.queue_player(ctx, title, player)
+                    await self.queue_player(ctx, title, FFmpegPCMAudio(f'./music/{title}'))
                 else:
                     await self.download(ctx, url)
                     title = self.get_title(ctx, url)
-                    await self.queue_player(ctx, title, FFmpegPCMAudio(f'./{title}'))
-                    ctx.voice_client.play(self.queue[0], after=lambda x: self.clean_up(title))
+                    await self.queue_player(ctx, title, FFmpegPCMAudio(f'./music/{title}'))
+                    await ctx.voice_client.play(self.queue[0], after=lambda x: self.clean_up(title))
                     await ctx.send(f'Now playing: {title}')
         else:
             await ctx.send('you must be in a voice channel to use this command!')
+
+    @commands.command()
+    async def join(self, ctx):
+        voice_clients = discord.utils.get(self.client.voice_clients, guild=ctx.guild) 
+        if voice_clients == None:
+            channel = ctx.message.author.voice.channel 
+            await channel.connect()
 
     @commands.command()
     async def leave(self, ctx):
@@ -81,10 +87,11 @@ class MusicPlayer(commands.Cog):
     @commands.command()
     async def skip(self, ctx):
         if ctx.message.author.voice:
-            if ctx.voice_clients.is_playing():
+            if ctx.voice_client.is_playing():
                 ctx.voice_client.stop()
-                self.queue.pop(0)
-                await ctx.voice_client.play(self.queue[0], after=lambda x: self.queue.pop(0))
+                self.clean_up()
+                await ctx.send('Skipping song!') 
+                await ctx.voice_client.play(self.queue[0], after=lambda x: self.clean_up())
             else:
                 await ctx.send('Nothing is playing in this channel')
         else:
