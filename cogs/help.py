a65b955a1d487282c3202b5564a12a0cfde834f8
Nova-archx86 <moorcode66@yahoo.com>
2023-01-19 13:36:49 -0800

    updated help command

--- cogs/help.py
+++ cogs/help.py
@@ -9,17 +9,18 @@ class Help(commands.Cog):
 
     @commands.command()
     async def help(self, ctx):
-        embed = discord.Embed(title='List of commands')
+        embed = discord.Embed(title='List of commands', color=discord.Color.random())
         embed.add_field(name='prefix', value='$', inline=False)
         embed.add_field(name='help', value='Sends this message', inline=False)
         embed.add_field(name='status', value='Gets the current status from the minecraft server (online players and '
                                              'latency)', inline=False)
         embed.add_field(name='pm', value='Randomly picks a map and difficulty for phasmophobia')
-        embed.add_field(name='play', value='Downloads and plays a youtube url using the yt-dlp project', inline=False)
+        embed.add_field(name='play <url>', value='Downloads and plays a youtube url using the yt-dlp project', inline=False)
         embed.add_field(name='pause', value='Pauses the current audio player', inline=False)
         embed.add_field(name='resume', value='Resumes a paused audio player', inline=False)
         embed.add_field(name='skip', value='Skips the current song in the queue', inline=False)
         embed.add_field(name='stop', value='Stops the current audio player and leaves the voice channel', inline=False)
+        embed.add_field(name='leave', value='disconnects from the current voice channel', inline=False)
         await ctx.send(embed=embed)
 
 
