96e8349b2583965098791525a6fbdc4ca5bf4f68
Nova-archx86 <moorcode66@yahoo.com>
2023-01-19 13:36:22 -0800

    added a check to see if the bot process is already running

--- start.sh
+++ start.sh
@@ -6,8 +6,16 @@ echo "Checking for ffmpeg..."
 
 if [[ -e /usr/bin/ffmpeg ]] || [[ -e /usr/local/bin/ffmpeg ]]
 then
-	echo "Starting..."
-	nohup python3 bot.py &
+	
+	if pgrep -fx 'python3 bot.py' >/dev/null
+	then
+		echo "ERROR bot is already running exiting..."
+		exit 1
+	else
+		echo "Starting..."	
+		nohup python3 bot.py &
+	fi
+
 else
 	echo "ERROR! ffmpeg is not installed!"
 fi
